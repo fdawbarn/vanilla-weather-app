029ca9e0548c7b5b7f6c7cdd825a972c574e5131
fdawbarn <86805111+fdawbarn@users.noreply.github.com>
2021-07-06 21:55:44 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -30,10 +30,26 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity; 
 windElement.innerHTML = Math.round(response.data.wind.speed); 
 dateElement.innerHTML = formatDate(response.data.dt*1000);
-iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }; 
+
+function search(city){
 let apiKey = "d0f75ef944c3443ad82f1c3cdedd03ad"; 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+}
 
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("london");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
